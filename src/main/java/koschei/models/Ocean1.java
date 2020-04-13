59b7989d98e9c94279920feedc8ebf2ae2c73392
Роман Андреевич Лукьянов <58958266+vienoulis@users.noreply.github.com>
2020-04-13 20:10:24 +0300

    test

---
+++ src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+
+    private final Island2 island;
+
+    @Autowired
+    public Ocean1(Island2 island) {
+        this.island = island;
+    }
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island.toString();
+    }
+}
